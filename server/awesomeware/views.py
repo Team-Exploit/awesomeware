a6c0fa21ca88bfbefcdbfb16e4085254b10cf7b5
Léo <leoraphael.poulin@epitech.eu>
2018-05-03 13:00:09 -0700

    added required routes

--- server/awesomeware/views.py
+++ server/awesomeware/views.py
@@ -9,3 +9,26 @@ def index():
 def toto():
     magic = "Toto is the best"
     return jsonify(magic)
+
+@app.route('/postpair', methods=['POST'])
+def post_key_pair():
+    try:
+        privatekey = request.form['privatekey']
+        publickey = request.form['publickey']
+    except KeyError:
+        return "You asshole"
+    # put the keys in database OR json
+    return jsonify({'status': 'ok'})
+
+@app.route('/getprivatekey', methods=['GET'])
+def get_private_key():
+    try:
+        publickey = request.headers['Publickey']
+    except KeyError:
+        return ('You asshole')
+    # Match public key
+    # Return privatekey
+    return jsonify({
+        'status': 'ok',
+        'privatekey': "12345678987654323456789"
+        })
