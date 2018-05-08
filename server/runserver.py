0415bde2f46b2b83de6aee9f57ace75b9cff1c2b
Ubuntu <ubuntu@ip-172-31-85-43.ec2.internal>
2018-05-08 17:57:40 +0000

    using ssl cert

--- server/runserver.py
+++ server/runserver.py
@@ -2,5 +2,9 @@
 from awesomeware import app
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', 5050)
+    app.run('0.0.0.0', 5050,
+            ssl_context=(
+                '/etc/letsencrypt/live/team-exploit.me/fullchain.pem',
+                '/etc/letsencrypt/live/team-exploit.me/privkey.pem'))
+
     
